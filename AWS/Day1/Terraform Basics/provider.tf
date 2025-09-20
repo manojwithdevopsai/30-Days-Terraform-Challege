56f9d71eb1b7146cdb2e2070f04ce8984b1b280e
Manoj Savukar <92372003+manojwithdevopsai@users.noreply.github.com>
2025-09-20 20:32:05 +0530

    Update provider.tf

--- AWS/Day1/Terraform Basics/provider.tf
+++ AWS/Day1/Terraform Basics/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.14.0"
+    }
+  }
+}
 
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "YOUR_ACCESS_KEY_ID"
+  secret_key = "YOUR_SECRET_ACCESS_KEY"
+}
